22be9163a0cb8832ff22517c412161c9cab5915a
cchcc <jo.cchcc@gmail.com>
2017-04-29 13:33:14 +0900

    용어정리

--- src/Main.hs
+++ src/Main.hs
@@ -1,55 +1,31 @@
 module Main where
 
---
--- 06. Higher order functions  http://learnyouahaskell.com/higher-order-functions
---
+
+-- 용어 & 개념 정리
+
+-- function applicatioin : 함수 호출.
+-- precedence : 함수호출이나 연산자등을 적용할때 뭐부터 할지에 대한 우선순위를 말함. 원 단어 뜻은 우선, 상위, 서열
+-- range : 리스트를 만들때 구성요소들의 순서나 규칙등을 정하는 방법
+-- type inference : 개발자가 일일이 이게 뭔 타입인지 명시하지 않아도 컴파일러가 알아서 무슨타입인지 인지함
+-- type declaration : 타입 선언(이름 :: 타입..)
+-- type variable : 타입 선언시 어떤 타입이든 관계없이 다 받을수 있다는 의미
+-- polymorphic function : type variable 을 가지고 있는 함수
+-- typeclass : 타입을 만들어내는 클래스. 상태는 없고 타입이 가져야 하는 행동만 있음. interface 비슷
+-- class constraint : 어떤 type variable 이 최소 어떤 typeclass 가 있어야 한다고 제약을 거는 방법.
+-- type annotation : 어떤 값이 무슨 타입인지 직접 명시해서 컴파일러에게 알려줌. 문법(::)이 타입 선언이랑 똑같음
+-- pattern matching : 어떤 데이터를 분해를 하거나, 분해해서 어떤 형태가 일치하는지 확인하는 방법. 함수의 바디 구현이나 where 구문에서 사용가능
+-- edge condition : 재귀가 끝나게 해주는 조건. 재귀 로직상 말이 안되는(더이상 재귀 수행이 안되는) 조건.
+-- partial application : 함수 호출시 모든 파라매터를 넣고 호출하는게 아니라 일부만 넣고 호출하는것.
+-- partially applied function : 부분적용해서 결과로 리턴된 함수
+-- curried function : 함수에 파라매터 1개만 적용에서 결과로 리턴된 함수
+-- module : typeclass 나 function 등 관련있는것들 끼리 모아서 만든 일종의 라이브러리
+-- prelude module : import 를 안해도 기본으로 탑재되어 있는 모듈들
+-- data declaration : 값을 가지고 있는 대수형 데이타 타입 선언
+-- value constructos : data 를 만들때 값을 받는 생성자
+-- record syntax : data 선언시 필드에 이름을 지정해주는 방법.
+-- type constructors : data 를 만들때 타입을 받는 생성자
+-- type synonyms : 똑같은 type 이 이름이 여러개 있는것.  ex) type String = [Char]
 
 
--- 하스켈은 함수의 기본 동작이 커링임 -> 파라매터를 다 안채워서 호출하면 부분 적용된 함수가 리턴됨
-plusXYZ x y z = x + y + z
-plus5YZ = plusXYZ 5     -- plusXYZ 의 내용이 부분 적용됨 : 5 + y + z
---main = print (plus5YZ 1 1 )
 
--- 커링함수 : 함수의 인자갯수가 1개이고 부분적용된 함수를 리턴함
--- 부분적용 함수 : 함수의 인자갯수와 상관없이 부분적용된 함수를 리턴
--- 부분함수 : 스칼라에 있는개념
-
--- Section of an infix operator. 이건 뭐 별 설명도 없이 갑툭튀야.. https://wiki.haskell.org/Section_of_an_infix_operator
-xbyten = (/10)       -- x / 10
-tenbyx = (10/)       -- 10 / x
---main = print ( tenbyx 100)
-sub10 = (subtract 10)   -- 다만 (-10) 의 경우 단항 연산으로 음수로 판단하므로 이경우에는 (subtract 10) 이렇게 사용함
---main = print (sub10 10)
-isUpperAlphanum = (`elem` ['A'..'Z'])   -- 사칙연산 말고도 이렇게도 사용가능
-
-
-applyTwice :: (a -> a) -> a -> a    -- high order function
-applyTwice f x = f (f x)
---main = print (applyTwice (+3) 3)
-
-zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]    -- (a -> b -> c) 이넘이 f
-zipWith' _ [] _ = []
-zipWith' _ _ [] = []
-zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
---zipWith' (zipWith' (*)) [[1,2,3],[3,5,6],[2,3,4]] [[3,2,2],[3,4,5],[5,4,3]]
-
-flip' :: (a -> b -> c) -> (b -> a -> c) -- 인풋이 (a -> b -> c) 리턴이 (b -> a -> c)
-flip' f = g where g x y = f y x
-flip'' :: (a -> b -> c) -> b -> a -> c  -- 인풋이 (a -> b -> c) 랑 b, a 리턴이 c
-flip'' f y x = f x y
---zipWith (flip' div) [2,2..] [10,8,6,4,2]
-
-
-filter' :: (a -> Bool) -> [a] -> [a]
-filter' _ [] = []
---filter' p (x:xs)
---    | p x       = x : filter p xs
---    | otherwise = filter p xs
-filter' p (x:xs) = if(p x) then x : filter' p xs else filter' p xs  -- 위에꺼랑 같음
---main = print(filter' even [1,2,3,4,5,6])
-
-funs = map (*) [0..]  -- 이건 리턴이 함수들의 리스트임 [(0*),(1*),(2*),(3*),(4*),(5*)...
---main = print ( (funs !! 3) 4)  -- (3*) 4
-
-
-main = print "hello world"
+main = print $
